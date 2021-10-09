6e4bf3b2e0cf9e721b0c77cfae057f19751dc9c2
bandgarmamta <bandgarmamta@gmail.com>
2021-10-10 00:20:08 +0530

    feat: added event listener

--- script.js
+++ script.js
@@ -1,3 +1,32 @@
 var inputNum = document.querySelector("#txt-input");
 var btnCheck = document.querySelector("#btn-check");
 var outputNum = document.querySelector("#output");
+
+btnCheck.addEventListener("click",isPrime)
+
+function isPrime(number){
+  var result = "";
+  var num = Number(inputNum.value);
+  if(num>=0){
+    if(num ===0 || num===1){
+    outputNum.textContent = "It's neither prime nor composite";
+  }
+    else if(num===2){
+    outputNum.textContent = "Yes! it is a prime number";
+  }
+  else {
+    for(var i= 2; i <= Math.sqrt(num); i++){  
+    if(num % i ==0){
+      outputNum.textContent = "No! it's not a prime number";  
+      break;
+    }else{
+      outputNum.textContent ="Yes! it is a prime number";
+    }
+  }
+  }
+
+  }else{
+    outputNum.textContent = "Please enter a valid number";
+  }
+  
+}
